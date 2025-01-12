f275978b5c0837c0a328e8b16a5dc9529b7def96
larycho <sample@users.noreply.com>
2025-01-12 11:19:06 +0100

    Poprawione namespace'y

--- Models/Commands/ICommand.cs
+++ Models/Commands/ICommand.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ZespolowyStateCommand.Commands
+namespace Battleships.Models.Commands
 {
     public interface ICommand
     {
