c8c7f73a741f2fb3aab9a16e3d30b23f635708aa
emilia-nodz <extra.maslo.game@gmail.com>
2025-01-13 01:10:14 +0100

    Dodano zmiany z brancha Singleton

---
+++ Models/Statistics.cs
@@ -0,0 +1,31 @@
+namespace Battleship.Models
+{
+    public class Statistics
+    {
+        public int PlayerID { get; set; }
+        public int GamesPlayed { get; private set; }
+        public int GamesWon { get; private set; }
+        public int GamesLost { get; private set; }
+
+        public Statistics(int playerId)
+        {
+            PlayerID = playerId;
+            GamesPlayed = 0;
+            GamesWon = 0;
+            GamesLost = 0;
+        }
+
+        public void RecordResult(bool isWin)
+        {
+            GamesPlayed++;
+            if (isWin)
+            {
+                GamesWon++;
+            }
+            else
+            {
+                GamesLost++;
+            }
+        }
+    }
+}
